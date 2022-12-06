cdf6ed99971ab582b5147e7b67711c66556dd6fc
Muhammad Asad Haider <mu979926@ucf.edu>
2022-12-06 17:38:39 -0500

    atom maker done

---
+++ Assets/Scripts/OnGrabParticle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class OnGrabParticle : MonoBehaviour
+{
+    void Update()
+    {
+        // check if object is grabbed
+        if (GetComponent<XRGrabInteractable>().isSelected)
+        {
+            // remove the random motion script
+            Destroy(GetComponent<RandomMotion>());
+        }
+    }
+}
